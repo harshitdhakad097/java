e518ad5faf5d0d223a35f6535035353424ddcbd0
Harshit Dhakad <harshitdhakad097@gmail.com>
2026-07-04 12:34:26 +0530

    cyclic sort

--- src/sorting/cyclicsort.java
+++ src/sorting/cyclicsort.java
@@ -1,8 +1,29 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class cyclicsort {
     public static void main(String[] args) {
+        int[]arr={7,8,9,4,5,6,1,2,3};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sort(int[] arr){
+     int i=0;
+     while(i<arr.length){
+         int correct =arr[i]-1;
+         if(arr[i]!=arr[correct]){
+             swap(arr,i,correct);
+         }else{
+             i++;
+         }
+     }
 
     }
+    public static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
-}
+    }
+
